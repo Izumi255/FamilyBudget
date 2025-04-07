513003a1e610629eabb260f3eed94ccec88e15db
Izumi255 <dimaburak428@gmail.com>
2025-04-07 15:47:17 +0300

    first commit

---
+++ familybudget/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace familybudget;
+
+public partial class App : Application
+{
+    private Window? window;
+
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        window = new MainWindow();
+        window.Activate();
+    }
+}
+
